23bc48d8bf0b54317833f523f1bfdcc3a8ac2382
Alexander Wesner <A.wesner@besecke.de>
2025-03-19 10:21:56 +0100

    GM-29 Create a page to register the user.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 }
